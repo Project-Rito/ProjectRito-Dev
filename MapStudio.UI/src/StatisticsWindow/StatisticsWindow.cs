51dd647c8fdce5a1e85e1de0cb3f0a1ad75342b4
S41L0R <60116462+S41L0R@users.noreply.github.com>
2021-11-24 11:12:27 -0500

    Translation for stats window & camera pos added

--- MapStudio.UI/src/StatisticsWindow/StatisticsWindow.cs
+++ MapStudio.UI/src/StatisticsWindow/StatisticsWindow.cs
@@ -16,15 +16,19 @@ namespace MapStudio.UI
 
         public override void Render()
         {
-            ImGui.Text($"Num Draw Calls: {ResourceTracker.NumDrawCalls}");
-            ImGui.Text($"Num Draw Triangles: {ResourceTracker.NumDrawTriangles}");
+            ImGui.Text($"{TranslationSource.GetText("DRAW_CALL_NUM")} {ResourceTracker.NumDrawCalls}");
+            ImGui.Text($"{TranslationSource.GetText("DRAW_TRI_NUM")} {ResourceTracker.NumDrawTriangles}");
 
-            ImGui.Text($"Num Effect DrawCalls: {ResourceTracker.NumEffectDrawCalls}");
-            ImGui.Text($"Num Effect Instances: {ResourceTracker.NumEffectInstances}");
-            ImGui.Text($"Num Effect Triangles: {ResourceTracker.NumEffectTriangles}");
+            ImGui.Text($"{TranslationSource.GetText("FX_DRAW_CALL_NUM")} {ResourceTracker.NumEffectDrawCalls}");
+            ImGui.Text($"{TranslationSource.GetText("FX_INST_NUM")} {ResourceTracker.NumEffectInstances}");
+            ImGui.Text($"{TranslationSource.GetText("FX_TRI_NUM")} {ResourceTracker.NumEffectTriangles}");
 
-            ImGui.Text($"Num Skeletal Anims: {SkeletalAnims}");
-            ImGui.Text($"Num Material Anims: {MaterialAnims}");
+            ImGui.Text($"{TranslationSource.GetText("SK_ANIM_NUM")} {SkeletalAnims}");
+            ImGui.Text($"{TranslationSource.GetText("MAT_ANIM_NUM")} {MaterialAnims}");
+
+            ImGui.Text($"{TranslationSource.GetText("CAM_POS_X")} {GLContext.ActiveContext.Camera.TargetPosition.X}");
+            ImGui.Text($"{TranslationSource.GetText("CAM_POS_Y")} {GLContext.ActiveContext.Camera.TargetPosition.Y}");
+            ImGui.Text($"{TranslationSource.GetText("CAM_POS_Z")} {GLContext.ActiveContext.Camera.TargetPosition.Z}");
         }
     }
 }
