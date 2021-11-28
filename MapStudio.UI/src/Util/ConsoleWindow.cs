416997a3e11c008d10c7604a51a5e13116ffd114
KillzXGaming <millernathan35@gmail.com>
2021-11-28 15:32:24 -0500

    Add console utility for displaying console.

---
+++ MapStudio.UI/src/Util/ConsoleWindow.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace MapStudio.UI
+{
+    public class ConsoleWindowUtil
+    {
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        const int SW_HIDE = 0;
+        const int SW_SHOW = 5;
+
+        public static void Hide()
+        {
+            var handle = GetConsoleWindow();
+            // Hide
+            ShowWindow(handle, SW_HIDE);
+        }
+
+        public static void Show()
+        {
+            var handle = GetConsoleWindow();
+            // Show
+            ShowWindow(handle, SW_SHOW);
+        }
+    }
+}
