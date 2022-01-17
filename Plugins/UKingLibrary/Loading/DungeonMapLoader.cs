9b6c0bd38d1c9ad8824fd5ebbf9595a1f6e6e684
S41L0R <60116462+S41L0R@users.noreply.github.com>
2022-01-17 10:17:37 -0500

    Dungeon loading adjustment

--- Plugins/UKingLibrary/Loading/DungeonMapLoader.cs
+++ Plugins/UKingLibrary/Loading/DungeonMapLoader.cs
@@ -8,6 +8,7 @@ using OpenTK;
 using MapStudio.UI;
 using CafeLibrary;
 using CafeLibrary.Rendering;
+using Toolbox.Core;
 
 namespace UKingLibrary
 {
@@ -39,8 +40,12 @@ namespace UKingLibrary
 
             //Load mubin data from sarc
             ProcessLoading.Instance.Update(20, 100, "Loading map units");
-            TeraTreeInstances = new ProdInfo(new MemoryStream(GetBlwp("TeraTree")));
-            ClusteringInstances = new ProdInfo(new MemoryStream(GetBlwp("Clustering")));
+            var teraTree = GetBlwp("TeraTree");
+            if (teraTree != null)
+                TeraTreeInstances = new ProdInfo(new MemoryStream(teraTree));
+            var clustering = GetBlwp("Clustering");
+            if (clustering != null)
+                ClusteringInstances = new ProdInfo(new MemoryStream(clustering));
 
             ProcessLoading.Instance.Update(0, 100, "Loading map files.");
 
@@ -48,8 +53,15 @@ namespace UKingLibrary
             GlobalData.LoadActorDatabase();
 
             //Static and dynamic actors
-            MapFiles.Add(new MapData(new MemoryStream(GetMubin("Static")), $"{DungeonName}_Static.smubin"));
-            MapFiles.Add(new MapData(new MemoryStream(GetMubin("Dynamic")), $"{DungeonName}_Dynamic.smubin"));
+            var staticFile = GetMubin("Static");
+            if (staticFile != null)
+                MapFiles.Add(new MapData(new MemoryStream(GetMubin("Static")), $"{DungeonName}_Static.smubin"));
+            var dynamicFile = GetMubin("Dynamic");
+            if (dynamicFile != null)
+                MapFiles.Add(new MapData(new MemoryStream(GetMubin("Dynamic")), $"{DungeonName}_Dynamic.smubin"));
+
+            if (dynamicFile == null && staticFile == null)
+                StudioLogger.WriteErrorException("yeah umm... can't really find any mubins....");
 
             //Load into muunt editor
             editor.Load(MapFiles);
@@ -57,38 +69,56 @@ namespace UKingLibrary
             //editor.LoadProd(TeraTreeInstances, $"{DungeonName}_TeraTree.sblwp");
 
             //Load model data into editor
-            var render = new BfresRender(new MemoryStream(GetModel()), $"DgnMrgPrt_{DungeonName}.sbfres", null);
-            render.Textures = BfresLoader.GetTextures(new MemoryStream(GetTexture()));
-            render.CanSelect = false;
-            GLFrameworkEngine.GLContext.ActiveContext.Scene.AddRenderObject(render);
-
-            render.IsVisibleCallback += delegate
+            var dungeonModel = GetModel();
+            if (dungeonModel != null)
             {
-              return MapMuuntEditor.ShowMapModel;
-            };
+                var render = new BfresRender(new MemoryStream(GetModel()), $"DgnMrgPrt_{DungeonName}.sbfres", null);
+                render.Textures = BfresLoader.GetTextures(new MemoryStream(GetTexture()));
+                render.CanSelect = false;
+                GLFrameworkEngine.GLContext.ActiveContext.Scene.AddRenderObject(render);
+
+                render.IsVisibleCallback += delegate
+                {
+                    return MapMuuntEditor.ShowMapModel;
+                };
+            }
         }
 
         private byte[] GetModel()
         {
-            var data = DungeonData.SarcData.Files[$"Model/DgnMrgPrt_{DungeonName}.sbfres"];
-            return Toolbox.Core.IO.YAZ0.Decompress(data);
+            byte[] data;
+            DungeonData.SarcData.Files.TryGetValue($"Model/DgnMrgPrt_{DungeonName}.sbfres", out data);
+            if (data != null)
+                return Toolbox.Core.IO.YAZ0.Decompress(data);
+            return null;
         }
 
         private byte[] GetTexture()
         {
-            var data = DungeonData.SarcData.Files[$"Model/DgnMrgPrt_{DungeonName}.Tex.sbfres"];
-            return Toolbox.Core.IO.YAZ0.Decompress(data);
+            byte[] data;
+            DungeonData.SarcData.Files.TryGetValue($"Model/DgnMrgPrt_{DungeonName}.Tex.sbfres", out data);
+            if (data != null)
+                return Toolbox.Core.IO.YAZ0.Decompress(data);
+            return null;
         }
 
         private byte[] GetMubin(string type) {
-            var data = DungeonData.SarcData.Files[$"Map/CDungeon/{DungeonName}/{DungeonName}_{type}.smubin"];
-            return Toolbox.Core.IO.YAZ0.Decompress(data);
+            byte[] data;
+            DungeonData.SarcData.Files.TryGetValue($"Map/CDungeon/{DungeonName}/{DungeonName}_{type}.smubin", out data);
+            DungeonData.SarcData.Files.TryGetValue($"Map/MainFieldDungeon/{DungeonName}/{DungeonName}_{type}.smubin", out data);
+            if (data != null)
+                return Toolbox.Core.IO.YAZ0.Decompress(data);
+            return null;
         }
 
         private byte[] GetBlwp(string type)
         {
-            var data = DungeonData.SarcData.Files[$"Map/CDungeon/{DungeonName}/{DungeonName}_{type}.sblwp"];
-            return Toolbox.Core.IO.YAZ0.Decompress(data);
+            byte[] data;
+            DungeonData.SarcData.Files.TryGetValue($"Map/CDungeon/{DungeonName}/{DungeonName}_{type}.sblwp", out data);
+            DungeonData.SarcData.Files.TryGetValue($"Map/MainFieldDungeon/{DungeonName}/{DungeonName}_{type}.sblwp", out data);
+            if (data != null)
+                return Toolbox.Core.IO.YAZ0.Decompress(data);
+            return null;
         }
 
         public void Save(Stream stream)
