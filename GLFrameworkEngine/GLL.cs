0fb2214e323bea299895ad624055eec3baac368f
S41L0R <60116462+S41L0R@users.noreply.github.com>
2022-02-12 15:21:31 -0500

    Add interception for more GL functions

--- GLFrameworkEngine/GLL.cs
+++ GLFrameworkEngine/GLL.cs
@@ -46,7 +46,7 @@ namespace GLFrameworkEngine
                     GL.BindVertexArray(array);
                 };
             }
-            else 
+            else
                 GL.BindVertexArray(array);
         }
 
@@ -85,6 +85,43 @@ namespace GLFrameworkEngine
             }
             else
                 GL.Enable(target, index);
+        }
+
+        public new static void Disable(EnableCap cap)
+        {
+            if (EnableInstancing)
+            {
+                ThisCall.Setup += delegate
+                {
+                    GL.Disable(cap);
+                };
+            }
+            else
+                GL.Disable(cap);
+        }
+        public new static void Disable(IndexedEnableCap target, int index)
+        {
+            if (EnableInstancing)
+            {
+                ThisCall.Setup += delegate
+                {
+                    GL.Disable(target, index);
+                };
+            }
+            else
+                GL.Disable(target, index);
+        }
+        public new static void Disable(IndexedEnableCap target, uint index)
+        {
+            if (EnableInstancing)
+            {
+                ThisCall.Setup += delegate
+                {
+                    GL.Disable(target, index);
+                };
+            }
+            else
+                GL.Disable(target, index);
         }
 
         public new static void DepthFunc(DepthFunction func)
@@ -149,6 +186,448 @@ namespace GLFrameworkEngine
             }
             else
                 GL.PolygonMode(materialFace, polygonMode);
+        }
+
+        public new static void PolygonOffset(float factor, float units)
+        {
+            if (EnableInstancing)
+            {
+                ThisCall.Setup += delegate
+                {
+                    GL.PolygonOffset(factor, units);
+                };
+            }
+            else
+                GL.PolygonOffset(factor, units);
+        }
+
+        public new static void BindBuffer(BufferTarget target, int buffer)
+        {
+            if (EnableInstancing)
+            {
+                ThisCall.Setup += delegate
+                {
+                    GL.BindBuffer(target, buffer);
+                };
+            }
+            else
+                GL.BindBuffer(target, buffer);
+        }
+        public new static void BindBuffer(BufferTarget target, uint buffer)
+        {
+            if (EnableInstancing)
+            {
+                ThisCall.Setup += delegate
+                {
+                    GL.BindBuffer(target, buffer);
+                };
+            }
+            else
+                GL.BindBuffer(target, buffer);
+        }
+
+        public new static void DeleteBuffer(int buffers)
+        {
+            if (EnableInstancing)
+            {
+                ThisCall.Setup += delegate
+                {
+                    GL.DeleteBuffer(buffers);
+                };
+            }
+            else
+                GL.DeleteBuffer(buffers);
+        }
+        public new static void DeleteBuffer(uint buffers)
+        {
+            if (EnableInstancing)
+            {
+                ThisCall.Setup += delegate
+                {
+                    GL.DeleteBuffer(buffers);
+                };
+            }
+            else
+                GL.DeleteBuffer(buffers);
+        }
+
+        public new static void DeleteVertexArray(int arrays)
+        {
+            if (EnableInstancing)
+            {
+                ThisCall.Setup += delegate
+                {
+                    GL.DeleteVertexArray(arrays);
+                };
+            }
+            else
+                GL.DeleteVertexArray(arrays);
+        }
+        public new static void DeleteVertexArray(uint arrays)
+        {
+            if (EnableInstancing)
+            {
+                ThisCall.Setup += delegate
+                {
+                    GL.DeleteVertexArray(arrays);
+                };
+            }
+            else
+                GL.DeleteVertexArray(arrays);
+        }
+
+        public new static void EnableVertexAttribArray(int index)
+        {
+            if (EnableInstancing)
+            {
+                ThisCall.Setup += delegate
+                {
+                    GL.EnableVertexAttribArray(index);
+                };
+            }
+            else
+                GL.EnableVertexAttribArray(index);
+        }
+        public new static void EnableVertexAttribArray(uint index)
+        {
+            if (EnableInstancing)
+            {
+                ThisCall.Setup += delegate
+                {
+                    GL.EnableVertexAttribArray(index);
+                };
+            }
+            else
+                GL.EnableVertexAttribArray(index);
+        }
+
+        public new static void DisableVertexAttribArray(int index)
+        {
+            if (EnableInstancing)
+            {
+                ThisCall.Setup += delegate
+                {
+                    GL.DisableVertexAttribArray(index);
+                };
+            }
+            else
+                GL.DisableVertexAttribArray(index);
+        }
+        public new static void DisableVertexAttribArray(uint index)
+        {
+            if (EnableInstancing)
+            {
+                ThisCall.Setup += delegate
+                {
+                    GL.DisableVertexAttribArray(index);
+                };
+            }
+            else
+                GL.DisableVertexAttribArray(index);
+        }
+
+        public new static void VertexAttribIPointer(int index, int size, VertexAttribIntegerType type, int stride, IntPtr pointer)
+        {
+            if (EnableInstancing)
+            {
+                ThisCall.Setup += delegate
+                {
+                    GL.VertexAttribIPointer(index, size, type, stride, pointer);
+                };
+            }
+            else
+                GL.VertexAttribIPointer(index, size, type, stride, pointer);
+        }
+        public new static void VertexAttribIPointer<T>(int index, int size, VertexAttribIntegerType type, int stride, [In] [Out] T[] pointer) where T : struct
+        {
+            if (EnableInstancing)
+            {
+                ThisCall.Setup += delegate
+                {
+                    GL.VertexAttribIPointer(index, size, type, stride, pointer);
+                };
+            }
+            else
+                GL.VertexAttribIPointer(index, size, type, stride, pointer);
+        }
+        public new static void VertexAttribIPointer<T>(int index, int size, VertexAttribIntegerType type, int stride, [In][Out] T[,] pointer) where T : struct
+        {
+            if (EnableInstancing)
+            {
+                ThisCall.Setup += delegate
+                {
+                    GL.VertexAttribIPointer(index, size, type, stride, pointer);
+                };
+            }
+            else
+                GL.VertexAttribIPointer(index, size, type, stride, pointer);
+        }
+        public new static void VertexAttribIPointer<T>(int index, int size, VertexAttribIntegerType type, int stride, [In][Out] ref T pointer) where T : struct
+        {
+            if (EnableInstancing)
+            {
+                throw new Exception("GLL: Something was passed by reference, but that's not supported by instancing!");
+            }
+            else
+                GL.VertexAttribIPointer(index, size, type, stride, ref pointer);
+        }
+        public new static void VertexAttribIPointer(uint index, int size, VertexAttribIntegerType type, int stride, IntPtr pointer)
+        {
+            if (EnableInstancing)
+            {
+                ThisCall.Setup += delegate
+                {
+                    GL.VertexAttribIPointer(index, size, type, stride, pointer);
+                };
+            }
+            else
+                GL.VertexAttribIPointer(index, size, type, stride, pointer);
+        }
+        public new static void VertexAttribIPointer<T>(uint index, int size, VertexAttribIntegerType type, int stride, [In][Out] T[] pointer) where T: struct
+        {
+            if (EnableInstancing)
+            {
+                ThisCall.Setup += delegate
+                {
+                    GL.VertexAttribIPointer(index, size, type, stride, pointer);
+                };
+            }
+            else
+                GL.VertexAttribIPointer(index, size, type, stride, pointer);
+        }
+        public new static void VertexAttribIPointer<T>(uint index, int size, VertexAttribIntegerType type, int stride, [In][Out] T[,] pointer) where T : struct
+        {
+            if (EnableInstancing)
+            {
+                ThisCall.Setup += delegate
+                {
+                    GL.VertexAttribIPointer(index, size, type, stride, pointer);
+                };
+            }
+            else
+                GL.VertexAttribIPointer(index, size, type, stride, pointer);
+        }
+        public new static void VertexAttribIPointer<T>(uint index, int size, VertexAttribIntegerType type, int stride, [In][Out] T[,,] pointer) where T : struct
+        {
+            if (EnableInstancing)
+            {
+                ThisCall.Setup += delegate
+                {
+                    GL.VertexAttribIPointer(index, size, type, stride, pointer);
+                };
+            }
+            else
+                GL.VertexAttribIPointer(index, size, type, stride, pointer);
+        }
+        public new static void VertexAttribIPointer<T>(uint index, int size, VertexAttribIntegerType type, int stride, [In][Out] ref T pointer) where T : struct
+        {
+            if (EnableInstancing)
+            {
+                throw new Exception("GLL: Something was passed by reference, but that's not supported by instancing!");
+            }
+            else
+                GL.VertexAttribIPointer(index, size, type, stride, ref pointer);
+        }
+
+        public new static void VertexAttribPointer(int index, int size, VertexAttribPointerType type, bool normalized, int stride, int offset)
+        {
+            if (EnableInstancing)
+            {
+                ThisCall.Setup += delegate
+                {
+                    GL.VertexAttribPointer(index, size, type, normalized, stride, offset);
+                };
+            }
+            else
+                GL.VertexAttribPointer(index, size, type, normalized, stride, offset);
+        }
+        public new static void VertexAttribPointer(int index, int size, VertexAttribPointerType type, bool normalized, int stride, IntPtr pointer)
+        {
+            if (EnableInstancing)
+            {
+                ThisCall.Setup += delegate
+                {
+                    GL.VertexAttribPointer(index, size, type, normalized, stride, pointer);
+                };
+            }
+            else
+                GL.VertexAttribPointer(index, size, type, normalized, stride, pointer);
+        }
+        public new static void VertexAttribPointer<T>(int index, int size, VertexAttribPointerType type, bool normalized, int stride, [In] [Out] T[] pointer) where T : struct
+        {
+            if (EnableInstancing)
+            {
+                ThisCall.Setup += delegate
+                {
+                    GL.VertexAttribPointer(index, size, type, normalized, stride, pointer);
+                };
+            }
+            else
+                GL.VertexAttribPointer(index, size, type, normalized, stride, pointer);
+        }
+        public new static void VertexAttribPointer<T>(int index, int size, VertexAttribPointerType type, bool normalized, int stride, [In][Out] T[,] pointer) where T : struct
+        {
+            if (EnableInstancing)
+            {
+                ThisCall.Setup += delegate
+                {
+                    GL.VertexAttribPointer(index, size, type, normalized, stride, pointer);
+                };
+            }
+            else
+                GL.VertexAttribPointer(index, size, type, normalized, stride, pointer);
+        }
+        public new static void VertexAttribPointer<T>(int index, int size, VertexAttribPointerType type, bool normalized, int stride, [In][Out] T[,,] pointer) where T : struct
+        {
+            if (EnableInstancing)
+            {
+                ThisCall.Setup += delegate
+                {
+                    GL.VertexAttribPointer(index, size, type, normalized, stride, pointer);
+                };
+            }
+            else
+                GL.VertexAttribPointer(index, size, type, normalized, stride, pointer);
+        }
+        public new static void VertexAttribPointer<T>(int index, int size, VertexAttribPointerType type, bool normalized, int stride, [In][Out] ref T pointer) where T : struct
+        {
+            if (EnableInstancing)
+            {
+                ThisCall.Setup += delegate
+                {
+                    throw new Exception("GLL: Something was passed by reference, but that's not supported by instancing!");
+                };
+            }
+            else
+                GL.VertexAttribPointer(index, size, type, normalized, stride, ref pointer);
+        }
+        public new static void VertexAttribPointer(uint index, int size, VertexAttribPointerType type, bool normalized, int stride, IntPtr pointer)
+        {
+            if (EnableInstancing)
+            {
+                ThisCall.Setup += delegate
+                {
+                    GL.VertexAttribPointer(index, size, type, normalized, stride, pointer);
+                };
+            }
+            else
+                GL.VertexAttribPointer(index, size, type, normalized, stride, pointer);
+        }
+        public new static void VertexAttribPointer<T>(uint index, int size, VertexAttribPointerType type, bool normalized, int stride, [In][Out] T[] pointer) where T : struct
+        {
+            if (EnableInstancing)
+            {
+                ThisCall.Setup += delegate
+                {
+                    GL.VertexAttribPointer(index, size, type, normalized, stride, pointer);
+                };
+            }
+            else
+                GL.VertexAttribPointer(index, size, type, normalized, stride, pointer);
+        }
+        public new static void VertexAttribPointer<T>(uint index, int size, VertexAttribPointerType type, bool normalized, int stride, [In][Out] T[,] pointer) where T : struct
+        {
+            if (EnableInstancing)
+            {
+                ThisCall.Setup += delegate
+                {
+                    GL.VertexAttribPointer(index, size, type, normalized, stride, pointer);
+                };
+            }
+            else
+                GL.VertexAttribPointer(index, size, type, normalized, stride, pointer);
+        }
+        public new static void VertexAttribPointer<T>(uint index, int size, VertexAttribPointerType type, bool normalized, int stride, [In][Out] T[,,] pointer) where T : struct
+        {
+            if (EnableInstancing)
+            {
+                ThisCall.Setup += delegate
+                {
+                    GL.VertexAttribPointer(index, size, type, normalized, stride, pointer);
+                };
+            }
+            else
+                GL.VertexAttribPointer(index, size, type, normalized, stride, pointer);
+        }
+        public new static void VertexAttribPointer<T>(uint index, int size, VertexAttribPointerType type, bool normalized, int stride, [In][Out] ref T pointer) where T : struct
+        {
+            if (EnableInstancing)
+            {
+                ThisCall.Setup += delegate
+                {
+                    throw new Exception("GLL: Something was passed by reference, but that's not supported by instancing!");
+                };
+            }
+            else
+                GL.VertexAttribPointer(index, size, type, normalized, stride, ref pointer);
+        }
+
+        public new static void VertexAttribDivisor(int index, int divisor)
+        {
+            if (EnableInstancing)
+            {
+                ThisCall.Setup += delegate
+                {
+                    GL.VertexAttribDivisor(index, divisor);
+                };
+            }
+            else
+                GL.VertexAttribDivisor(index, divisor);
+        }
+        public new static void VertexAttribDivisor(uint index, uint divisor)
+        {
+            if (EnableInstancing)
+            {
+                ThisCall.Setup += delegate
+                {
+                    GL.VertexAttribDivisor(index, divisor);
+                };
+            }
+            else
+                GL.VertexAttribDivisor(index, divisor);
+        }
+
+        public new static void GenVertexArrays(int n, [Out] int[] arrays)
+        {
+            if (EnableInstancing)
+            {
+                ThisCall.Setup += delegate
+                {
+                    GL.GenVertexArrays(n, arrays);
+                };
+            }
+            else
+                GL.GenVertexArrays(n, arrays);
+        }
+        public new static void GenVertexArrays(int n, out int arrays)
+        {
+            if (EnableInstancing)
+            {
+                throw new Exception("GLL: Something was passed by reference, but that's not supported by instancing!");
+            }
+            else
+                GL.GenVertexArrays(n, out arrays);
+        }
+        public new static void GenVertexArrays(int n, [Out] uint[] arrays)
+        {
+            if (EnableInstancing)
+            {
+                ThisCall.Setup += delegate
+                {
+                    GL.GenVertexArrays(n, arrays);
+                };
+            }
+            else
+                GL.GenVertexArrays(n, arrays);
+        }
+        public new static void GenVertexArrays(int n, out uint arrays)
+        {
+            if (EnableInstancing)
+            {
+                throw new Exception("GLL: Something was passed by reference, but that's not supported by instancing!");
+            }
+            else
+                GL.GenVertexArrays(n, out arrays);
         }
 
         // Draw Functions
