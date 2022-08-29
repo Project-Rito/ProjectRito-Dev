15c1de2276e8099edd103addc4d9f8e07500f6db
S41L0R <60116462+S41L0R@users.noreply.github.com>
2022-08-29 18:58:57 -0400

    Overworld navmesh gen

--- Plugins/UKingLibrary/Tools/NavmeshBuilder.cs
+++ Plugins/UKingLibrary/Tools/NavmeshBuilder.cs
@@ -25,37 +25,54 @@ namespace UKingLibrary
 
         public static void Build()
         {
-            DrawingHelper.VerticesIndices<Vector3> mergedMesh = GetMergedMesh();
+            foreach (var navmeshLoader in ActiveLoader.Navmesh)
+            {
+                Vector3 min = navmeshLoader.Origin - new Vector3(250/2);
+                min.Y = float.MinValue;
+                Vector3 max = navmeshLoader.Origin + new Vector3(250/2);
+                max.Y = float.MaxValue;
+                DrawingHelper.VerticesIndices<Vector3> mergedMesh = GetMergedMesh(navmeshLoader.Origin, min, max);
 
-            var navmesh = hkaiNavMeshBuilder.Build(UKingEditor.ActiveUkingEditor.EditorConfig.NavmeshConfig, mergedMesh.Vertices.Select(v => new System.Numerics.Vector3(v.X, v.Y, v.Z)).ToList(), mergedMesh.Indices);
+                var navmesh = hkaiNavMeshBuilder.Build(UKingEditor.ActiveUkingEditor.EditorConfig.NavmeshConfig, mergedMesh.Vertices.Select(v => new System.Numerics.Vector3(v.X, v.Y, v.Z)).ToList(), mergedMesh.Indices);
 
-
-            ActiveLoader.Navmesh[0].Replace(navmesh);
+                navmeshLoader.Replace(navmesh);
+            }
         }
 
         /// <summary>
         /// Gets a simple merged mesh from all the collision data in the current loader/scene.
         /// </summary>
-        private static DrawingHelper.VerticesIndices<Vector3> GetMergedMesh()
+        private static DrawingHelper.VerticesIndices<Vector3> GetMergedMesh(Vector3 navmeshOrigin, Vector3 sampleMin, Vector3 sampleMax)
         {
             DrawingHelper.VerticesIndices<Vector3> res = new DrawingHelper.VerticesIndices<Vector3>();
             int vtxBase = 0;
-            foreach (var render in ActiveLoader.BakedCollision[0].ShapeRenders)
+            foreach (var collisionLoader in ActiveLoader.BakedCollision)
             {
-                Matrix4 renderTransform = render.Transform.TransformMatrix;
-                renderTransform.Transpose();
-
-                Matrix4 navmeshTransform = Matrix4.CreateTranslation(ActiveLoader.Navmesh[0].Origin * GLContext.PreviewScale);
-                navmeshTransform.Transpose();
-
-                res.Vertices.AddRange(render.Vertices.Select(x => (navmeshTransform.Inverted() * renderTransform * new Vector4(x.Position, 1f)).Xyz / GLContext.PreviewScale));
-
-                foreach (int index in render.Indices)
+                foreach (var render in collisionLoader.ShapeRenders)
                 {
-                    res.Indices.Add(index + vtxBase);
-                }
+                    if (!(render.Transform.Position.X / GLContext.PreviewScale > sampleMin.X &&
+                        render.Transform.Position.Y / GLContext.PreviewScale > sampleMin.Y &&
+                        render.Transform.Position.Z / GLContext.PreviewScale > sampleMin.Z &&
+                        render.Transform.Position.X / GLContext.PreviewScale <= sampleMax.X &&
+                        render.Transform.Position.Y / GLContext.PreviewScale <= sampleMax.Y &&
+                        render.Transform.Position.Z / GLContext.PreviewScale <= sampleMax.Z))
+                        continue;
 
-                vtxBase += render.Vertices.Length;
+                    Matrix4 renderTransform = render.Transform.TransformMatrix;
+                    renderTransform.Transpose();
+
+                    Matrix4 navmeshTransform = Matrix4.CreateTranslation(navmeshOrigin * GLContext.PreviewScale);
+                    navmeshTransform.Transpose();
+
+                    res.Vertices.AddRange(render.Vertices.Select(x => (navmeshTransform.Inverted() * renderTransform * new Vector4(x.Position, 1f)).Xyz / GLContext.PreviewScale));
+
+                    foreach (int index in render.Indices)
+                    {
+                        res.Indices.Add(index + vtxBase);
+                    }
+
+                    vtxBase += render.Vertices.Length;
+                }
             }
 
             return res;
